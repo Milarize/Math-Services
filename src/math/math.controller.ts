1592334096dd6f5f136ef2aecf8d56c343faece6
Kulisara Thongngamkhum <64160233@go.buu.ac.th>
2024-09-08 17:20:58 +0700

    ✅ Success call sum message

--- src/math/math.controller.ts
+++ src/math/math.controller.ts
@@ -1,5 +1,6 @@
 import { Body, Controller, Logger, Post } from '@nestjs/common';
 import { MathService } from './math.service';
+import { MessagePattern } from '@nestjs/microservices';
 
 @Controller('math')
 export class MathController {
@@ -7,7 +8,13 @@ export class MathController {
   constructor(private readonly mathservice: MathService) {}
   @Post('sum')
   sum(@Body('numbers') numbers: number[]): number {
-    this.logger.log('sum' + numbers);
+    this.logger.log('sum ' + numbers);
+    return this.mathservice.accumulate(numbers);
+  }
+
+  @MessagePattern({ cmd: 'sum' })
+  sum2(numbers: number[]): number {
+    this.logger.log('sum2 ' + numbers);
     return this.mathservice.accumulate(numbers);
   }
 }
