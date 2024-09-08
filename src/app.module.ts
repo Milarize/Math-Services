6f289b130898c63739d5f18a58f17e2f2fc4867a
Kulisara Thongngamkhum <64160233@go.buu.ac.th>
2024-09-08 16:49:03 +0700

    ✅ Success Math_service

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MathController } from './math/math.controller';
+import { MathService } from './math/math.service';
 
 @Module({
   imports: [],
   controllers: [AppController, MathController],
-  providers: [AppService],
+  providers: [AppService, MathService],
 })
 export class AppModule {}
--- src/math/math.controller.ts
+++ src/math/math.controller.ts
@@ -1,4 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Logger, Post } from '@nestjs/common';
+import { MathService } from './math.service';
 
 @Controller('math')
-export class MathController {}
+export class MathController {
+  private logger = new Logger('MathController');
+  constructor(private readonly mathservice: MathService) {}
+  @Post('sum')
+  sum(@Body('numbers') numbers: number[]): number {
+    this.logger.log('sum' + numbers);
+    return this.mathservice.accumulate(numbers);
+  }
+}
